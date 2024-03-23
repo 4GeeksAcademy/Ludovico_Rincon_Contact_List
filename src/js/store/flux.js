5e998c960e7b4398f14a6ba0523601da46a92629
LRN251 <150847992+LRN251@users.noreply.github.com>
2024-03-23 12:10:05 +0000

    First commit feature feature/Contact_List

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,78 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contacts: [] // Empty Array
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getContacts: () => {
+				fetch('https://playground.4geeks.com/apis/fake/contact/agenda/LRN')
+					.then(resp => {
+						console.log("is response succesful: " + resp.ok); 
+						console.log("status code: " + resp.status); 
+						return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+					})
+					.then(data => {
+						console.log(data); //this will print on the console the exact object received from the server
+						setStore({ contacts: data })
+						console.log(getStore().contacts)
+					})
+					.catch(error => {
+						//error handling
+						console.log(error);
+					});
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+
+			uploadContact: (objContact) => {
+				fetch(`https://playground.4geeks.com/apis/fake/contact`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(objContact)
+				})
+					.then(response => {
+						console.log("uplaod response: ", response)
+						console.log("uplaod JSON:", response.json())
+						getActions().getContacts()
+					})
+					.catch(error => console.log(error))
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			editContact: (objContact) => {
+				fetch(`https://playground.4geeks.com/apis/fake/contact/${objContact.id}`, {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(objContact)
+				})
+					.then(response => {
+						console.log("uplaod response: ", response)
+						console.log("uplaod JSON:", response.json())
+						getActions().getContacts()
+					})
+					.catch(error => console.log(error))
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
+			deleteContact: (id) => {
+				console.log("deleteID", id)
+				fetch(`https://playground.4geeks.com/apis/fake/contact/${id}`, {
+					method: "DELETE",
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(response => {
+						console.log("uplaod response: ", response)
+						console.log("uplaod JSON:", response.json())
+						getActions().getContacts()
+					})
+					.catch(error => console.log(error))
 			}
-		}
-	};
-};
+
+		} // End actions
+	}; // End Return
+}; // End Function getState
 
 export default getState;
